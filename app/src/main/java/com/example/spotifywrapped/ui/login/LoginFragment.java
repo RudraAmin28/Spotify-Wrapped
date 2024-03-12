2f4d3e3d96b92cf38fe0762f5551200b4e13e91d
aryan-chokshi <aryanchokshi2004@gmail.com>
2024-03-11 23:34:08 -0400

    login/signup base ui

--- app/src/main/java/com/example/spotifywrapped/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/spotifywrapped/ui/login/LoginFragment.java
@@ -1,4 +1,4 @@
-package com.example.spotifywrapped.ui.home;
+package com.example.spotifywrapped.ui.login;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.spotifywrapped.databinding.FragmentHomeBinding;
+import com.example.spotifywrapped.databinding.FragmentLoginBinding;
 
-public class HomeFragment extends Fragment {
+public class LoginFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentLoginBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        LoginViewModel loginViewModel =
+                new ViewModelProvider(this).get(LoginViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textViewAppName;
+        loginViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
