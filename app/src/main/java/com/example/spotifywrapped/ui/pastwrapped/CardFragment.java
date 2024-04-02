68771b4b86d2141d45433e6cd735316445caacab
Sachi Kataria <sachikataria8@gmail.com>
2024-04-01 21:07:55 -0400

    cleanup commit

--- app/src/main/java/com/example/spotifywrapped/ui/pastwrapped/CardFragment.java
+++ app/src/main/java/com/example/spotifywrapped/ui/pastwrapped/CardFragment.java
@@ -1,18 +1,26 @@
 package com.example.spotifywrapped.ui.pastwrapped;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.spotifywrapped.databinding.FragmentCardBinding;
+import com.example.spotifywrapped.R;
+import com.example.spotifywrapped.ui.wrapped.WrappedFragment;
 
 public class CardFragment extends Fragment {
     private FragmentCardBinding binding;
+    private CardView currentYearCardView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -21,9 +29,17 @@ public class CardFragment extends Fragment {
 
         binding = FragmentCardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        //final TextView textView = binding.currentYear;
-        //cardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
+    }
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.currentYearCard).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 
     @Override
