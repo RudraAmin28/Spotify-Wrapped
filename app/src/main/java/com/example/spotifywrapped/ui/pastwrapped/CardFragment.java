e2351528eaf28ad9ce8672bdcf6f882439e685af
Sachi Kataria <sachikataria8@gmail.com>
2024-04-01 21:20:15 -0400

    sachi3 commit

--- app/src/main/java/com/example/spotifywrapped/ui/pastwrapped/CardFragment.java
+++ app/src/main/java/com/example/spotifywrapped/ui/pastwrapped/CardFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,9 +16,8 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.spotifywrapped.databinding.FragmentCardBinding;
 import com.example.spotifywrapped.R;
-import com.example.spotifywrapped.ui.wrapped.WrappedFragment;
+import com.example.spotifywrapped.databinding.FragmentCardBinding;
 
 public class CardFragment extends Fragment {
     private FragmentCardBinding binding;
@@ -29,7 +30,8 @@ public class CardFragment extends Fragment {
 
         binding = FragmentCardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
+        //final TextView textView = binding.currentYear;
+        //cardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
